b597fd1ec3e8aeae684c4812f9c19943c91f0c6b
siloratti <karolis.stoskus@gmail.com>
2025-04-12 13:46:52 +0000

    first commit

---
+++ src/main/java/lt/vu/PirmasKomponentas.java
@@ -0,0 +1,27 @@
+package lt.vu;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.util.Date;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class PirmasKomponentas {
+
+    public String SakykLabas() {
+        return "Labas " + new Date() + " " + toString();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println(toString()+ " constructed.");
+    }
+
+    @PreDestroy
+    public void aboutToDie() {
+        System.out.println(toString()+ " ready to die.");
+    }
+}
